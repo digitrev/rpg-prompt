7d01d503511deae1a530b074743f4ca8e657031d
Trevor Burn <digitrev@gmail.com>
2021-12-20 19:42:24 -0500

    Delta

--- rpg/src/App.tsx
+++ rpg/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
           window.location.reload();
         }}
       >
-        Generate another?
+        Generate another
       </button>
       <p>
         Source:{' '}
